7cd870cf3ea4341dc5bf430e1807bec3ceb285fa
ZEXXO <egiihaw99@gmail.com>
2022-04-01 07:12:56 +0700

    gatau

---
+++ shop/sewa.js
@@ -0,0 +1,44 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+╭──「 📖  *PRICE SEWABOT*
+│ 1 MINGGU = Rp 3.000
+│ 2 MINGGU = Rp 5.000
+│PERMANEN = Rp 7.000
+│PERMANEN + USER PREM = Rp 10.000
+│JASA RUN HEROKU BAWA DANA 20K+SC Sendiri
+│JASA RUN TERMUX BAWA DANA 20K+SC Sendiri
+
+╭──「 📖  *PRICE JADIBOT*
+│
+├ • *Permanen*
+│
+│ ⤷ *Harga :  25k [E-Wallet]*
+│ ⤷ *Harga :  30k [Pulsa]*
+│
+├─ 「 💰  *PAYMENT* 」
+│
+│ ⤷ *Dana [Diutamakan]*
+│ ⤷ *Ovo*
+│ ⤷ *Gopay*
+│ ⤷ *Pulsa Smartfren/Indosat*
+│
+├─ 「 👨‍💻  *RUN ON* 」
+│
+│ ⤷ *Heroku [Diutamakan]*
+│ ⤷ *Railway* 
+│
+├─ 「 📝  *NOTES* 」
+│
+│ ⤷ _Tidak perlu termux_
+│ ⤷ _Minat? Hubungi Wa.me//6289529291185_
+│
+╰──「 *Thank's* 」
+
+
+NOTE : 
+Bot On 24 Jam Karna Di Run Menggunakan Heroku
+Reset Data Setiap Jam 02:00 `
+	}
+
+exports.pc_sewa = pc_sewa
